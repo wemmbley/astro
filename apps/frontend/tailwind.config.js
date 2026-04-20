8fe25009788b9375e8b32b456888994d876fd5e1
Rustam <golevv.rustam@gmail.com>
2026-04-20 16:43:36 +0300

    fixed tailwind.

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./index.html",
         "./public/index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./resources/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
