6a4c117e113beda15bacab269cf6b2360f846040
Rustam <golevv.rustam@gmail.com>
2026-04-20 21:38:53 +0300

    added next; removed react infrastructure.

--- apps/frontend/app/Requests/About.ts
+++
@@ -1,6 +0,0 @@
-import { object, string, number, date, InferType } from 'yup';
-
-export const aboutSchema = object({
-    title: string().required(),
-    content: string().required(),
-});
--- apps/frontend/resources/views/Pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from 'react-router'
-
-export const Index = () => {
-    return <>
-        <h1 className="text-red-500 text-3xl font-bold">Hello, Index!</h1>
-        <Link to="/about">About</Link>
-    </>
-}
