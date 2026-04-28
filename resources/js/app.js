8f33cb90444eebf35915f187919872481e81f5ad
Rustam <golevv.rustam@gmail.com>
2026-04-28 16:52:24 +0300

    expansed expositor; added interia.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-//
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+    pages: {
+        path: './Pages',
+        extension: '.vue',
+        lazy: true,
+        transform: (name, page) => name.replace('/', '-'),
+    },
+})
---
+++ resources/views/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hello, Home!</h1>
+</template>
+
+<style scoped>
+
+</style>
